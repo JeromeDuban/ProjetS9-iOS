f7a95e0ad2cb7bd3521c74bd3763175a05442754
Jérémie Foucault <foucaultjeremie@gmail.com>
2014-12-21 11:51:54 +0100

    Display, drag and zoom RDC.svg map

--- Projet S9/MapViewController.swift
+++ Projet S9/MapViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-@objc(MapViewController) class MapViewController: UIViewController, UIScrollViewDelegate {
+class MapViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate {
     
     
-    @IBOutlet weak var svgScroll: UIScrollView!
+    @IBOutlet weak var svgScrollView: UIScrollView!
+    var svgFile: SVGKImage = SVGKImage(named: "RDC.svg")
+    var svgMap: SVGKImageView = SVGKFastImageView(SVGKImage: SVGKImage(named: "RDC.svg"))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Map"
         
+        self.slidingViewController().panGesture.delegate = self
+        self.view.addGestureRecognizer(self.slidingViewController().panGesture)
+
+        self.svgScrollView.sizeToFit()
+        self.svgScrollView.minimumZoomScale = 0.25
+        self.svgScrollView.maximumZoomScale = 2.5
+        self.svgScrollView.showsHorizontalScrollIndicator = false
+        self.svgScrollView.showsVerticalScrollIndicator = false
+        self.svgScrollView.delegate = self
+        
+        self.displayFloor(1)
+        svgMap = SVGKFastImageView(SVGKImage: svgFile)
+        self.svgScrollView.addSubview(svgMap)
+        self.svgScrollView.zoomToRect(svgMap.bounds, animated: false)
         
     }
     
@@ -26,6 +42,43 @@ import UIKit
         // Dispose of any resources that can be recreated.
     }
     
-
+    func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer!) -> Bool {
+        if (gestureRecognizer.locationInView(gestureRecognizer.view).x < 30.0) {
+            return true
+        }
+        return false
+    }
+    
+    
+    func displayFloor(index: Int) {
+        switch (index) {
+        case 0:
+            self.svgFile = SVGKImage(named: "SS.svg")
+            break
+        case 1:
+            self.svgFile = SVGKImage(named: "RDC.svg")
+            break
+        case 2:
+            self.svgFile = SVGKImage(named: "RDC+1.svg")
+            break
+        case 3:
+            self.svgFile = SVGKImage(named: "RDC+2.svg")
+            break
+        case 4:
+            self.svgFile = SVGKImage(named: "RDC+3.svg")
+            break
+        default:
+            self.svgFile = SVGKImage(named: "RDC.svg")
+            break
+        }
+        
+        self.svgMap.image = self.svgFile
+        self.svgScrollView.sizeToFit()
+        self.svgScrollView.zoomToRect(self.svgMap.bounds, animated: false)
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView!) -> UIView! {
+        return self.svgMap
+    }
     
 }
