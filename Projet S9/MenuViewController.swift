07bf0b6606cae96440088c159eae67b53cfe6503
gschahl <schahl.g@gmail.com>
2015-01-05 18:40:13 +0100

    Add calendar in menu

--- Projet S9/MenuViewController.swift
+++ Projet S9/MenuViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MenuViewController: UITableViewController {
     
     
-    var menuItems = [MenuItem(title: "Home", identifier: "HomeViewNavigation", image: UIImage(named: "Home")!), MenuItem(title: "Map", identifier: "MapViewNavigation", image: UIImage(named: "Map")!)]
+    var menuItems = [MenuItem(title: "Home", identifier: "HomeViewNavigation", image: UIImage(named: "Home")!), MenuItem(title: "Map", identifier: "MapViewNavigation", image: UIImage(named: "Map")!),MenuItem(title: "Calendar", identifier: "MapViewNavigation", image: UIImage(named: "Calendar")!)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
